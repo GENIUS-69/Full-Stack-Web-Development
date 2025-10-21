16de7f1f4c8cb85d84fa7de75577ca188a741cad
GENIUS-69 <galaxygaming0511@gmail.com>
2025-10-21 11:54:50 +0530

    Back on Track

---
+++ ReactJs/basicvite/src/main.jsx
@@ -0,0 +1,35 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const reactElement = {
+  type: 'a',
+  props: {
+    href: "https://google.com",
+    target: '_blank'
+  },
+  children: 'Click to Visit Google'
+}
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React App</h1>
+    </div>
+  )
+}
+
+const AnotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+  'a',
+  {href: 'https://google.com',target: '_blank'},
+  'click to visit'
+)
+
+createRoot(document.getElementById('root')).render(
+  areactElement
+)
---
+++ ReactJs/tailwindprops/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <img src="https://images.pexels.com/photos/18264716/pexels-photo-18264716.jpeg" alt="" />
+      <h1 className="text-2xl bg-green-500 p-3 rounded">Card For Photos</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, sapiente.</p>
+    </div>
+  )
+}
+
+export default Card
