3bdae231b01c293b93805eac008f4251ab879924
GENIUS-69 <galaxygaming0511@gmail.com>
2025-11-23 18:34:31 +0530

    skipping next js bcz of version issue

---
+++ NextJS/next-auth/src/app/profile/[id]/page.tsx
@@ -0,0 +1,15 @@
+export default function UserProfile({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <p className="text-4xl">
+        Profile Info: <span>{params.id}</span>
+      </p>
+    </div>
+  );
+}
