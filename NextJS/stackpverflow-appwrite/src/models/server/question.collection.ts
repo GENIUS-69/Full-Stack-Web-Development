46b7aa59b4706afc80bc0e80728ce1be0e10763e
GENIUS-69 <galaxygaming0511@gmail.com>
2025-12-02 20:20:37 +0530

    Server and Client configuration

---
+++ NextJS/stackpverflow-appwrite/src/models/server/question.collection.ts
@@ -0,0 +1,24 @@
+import { Permission } from 'node-appwrite'
+
+import { db, questionCollection } from '../name'
+import { databases } from './config'
+
+export default async function createQuestionCollection() {
+  await databases.createCollection(db, questionCollection, questionCollection, [
+    Permission.read("any"),
+    Permission.read("users"),
+    Permission.create("users"),
+    Permission.update("users"),
+    Permission.delete("users"),
+  ])
+  console.log("Question Collection Ceated");
+
+  // create attribute and index
+
+  await Promise.all([
+    databases.createStringAttribute(db, questionCollection, "title", 100, true),
+    databases.createStringAttribute(db, questionCollection, "content", 10000, true),
+    databases.createStringAttribute(db, questionCollection, "authorId", 50, true),
+    databases.createStringAttribute(db, questionCollection, "tags", 50, true, undefined, true),
+  ])
+}
